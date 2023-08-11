110efea5b338904c7113415a08637c4fbb9a7350
Nicholas Cunningham <ndcunningham@gmail.com>
2023-08-11 08:33:37 -0600

    Init

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   cacheDir: './node_modules/.vite/svelet-test',
 
-  plugins: [nxViteTsPaths()],
+  plugins: [
+    svelte(),
+    nxViteTsPaths()
+  ],
 
   // Uncomment this if you are using workers.
   // worker: {
